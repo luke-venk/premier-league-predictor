d142b78d9070678207442ea847f8ca80a418d3c7
Luke Venkataramanan <luke.venkataramanan@utexas.edu>
2026-01-24 20:20:11 -0600

    refactor: move data formatting functions to standalone module

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -6,12 +6,11 @@ returns function outputs to HTTP responses.
 """
 
 from fastapi import APIRouter
-from datetime import datetime
-from zoneinfo import ZoneInfo
 
 from backend.db.connection import get_connection
-from backend.api.schemas import Match, ProbOut, MatchOut, MatchResponse, TableResponse
+from backend.api.schemas import Match, MatchOut, TableResponse
 from backend.api.simulation_store import load_simulation, save_simulation
+from backend.api.interface import db_to_ui_matches
 from backend.sim.predictor import Predictor
 from backend.sim.generate_table import compute_standings
 from backend.db.simulations import list_simulations, create_simulation
@@ -54,7 +53,6 @@ def get_simulations() -> list[dict]:
     Returns a list of simulation objects. Each entry will be a dictionary
     containing simulation ID's and timestamps created.
     """
-    # TODO: add a dropdown to select from here
     simulations = list_simulations(conn)
     return simulations
 
@@ -66,41 +64,15 @@ def get_latest_simulation_id():
     
 # Functions to get the matches associated with a specific simulation.
 
-def row_to_match_out(r: dict) -> MatchOut:
-    """
-    Converts PostgreSQL outputs into data format immediately
-    acceptable by the frontend's UI.
-    """
-    return MatchOut(
-        id=r["id"],
-        date=r["match_date"],
-        homeId=r["home_id"],
-        awayId=r["away_id"],
-        probabilities=ProbOut(
-            homeWin=r["p_home"],
-            draw=r["p_draw"],
-            awayWin=r["p_away"]
-        ),
-        prediction=r["prediction"],
-        actual=r["actual"]
-    )
-
 @router.get("/matches", response_model=list[MatchOut])
 def get_matches():
     """
     Read match results from the latest simulation results.
     If there is no such file, return an empty list.
     """
-    # TODO: activate loading from PSQL
-    load_from_psql = True
-    if not load_from_psql:
-        # Read simulation from file.
-        payload = load_simulation()
-        return payload
-    else:
-        simulation_id = get_latest_simulation_id()
-        rows = get_predictions(conn, simulation_id)
-        return [row_to_match_out(r) for r in rows]
+    simulation_id = get_latest_simulation_id()
+    rows = get_predictions(conn, simulation_id)
+    return [db_to_ui_matches(r) for r in rows]
         
 
 @router.get("/table", response_model=TableResponse)
